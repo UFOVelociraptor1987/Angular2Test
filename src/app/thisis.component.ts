4c490806a1b263df0f2c90d678a440acaf8f546b
Ilya Tkachev <honeytar@yandex.ru>
2017-06-19 11:00:45 +0300

    ComponentName modification

--- src/app/thisis.component.ts
+++ src/app/thisis.component.ts
@@ -2,8 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'thisis',
-    template: `<h2>This is component</h2>`,
+    template: `<h2>This is {{ componentName }}</h2>`,
     styles: [`h2, p {color:red;}`]
 })
 export class ThisIsComponent {
+  componentName = "Sparta";
 }
