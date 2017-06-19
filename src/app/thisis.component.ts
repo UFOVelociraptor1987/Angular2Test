322abbbf33043bbddffbecb8a92c2628dbe5c625
Ilya Tkachev <honeytar@yandex.ru>
2017-06-19 19:16:01 +0300

    ComponentName modification

--- src/app/thisis.component.ts
+++ src/app/thisis.component.ts
@@ -4,13 +4,13 @@ import { Input, Output, EventEmitter } from '@angular/core';
 @Component({
     selector: 'thisis',
     template: `<h2>This is {{ componentName }}</h2>
-               <button (click)="ShowLog(true)">ZAP!</button>`,
+               <button (click)="ShowLog('Valera')">ZAP!</button>`,
     styles: [`h2, p {color:red;}`]
 })
 export class ThisIsComponent {
   @Input() componentName: string;
   @Output() onChanged = new EventEmitter<boolean>();
-  ShowLog(increased) {
-      console.log("Hello!");
+  ShowLog(name) {
+      console.log("Hello "+name+"!");
   }
 }
