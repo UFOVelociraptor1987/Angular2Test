060042e26352afb7c33f00b5678fde433c7663fe
Ilya Tkachev <honeytar@yandex.ru>
2017-06-19 19:11:19 +0300

    Output function

--- src/app/thisis.component.ts
+++ src/app/thisis.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
-import { Input} from '@angular/core';
+import { Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
     selector: 'thisis',
-    template: `<h2>This is {{ componentName }}</h2>`,
+    template: `<h2>This is {{ componentName }}</h2>
+               <button (click)="ShowLog(true)">ZAP!</button>`,
     styles: [`h2, p {color:red;}`]
 })
 export class ThisIsComponent {
   @Input() componentName: string;
+  @Output() onChanged = new EventEmitter<boolean>();
+  ShowLog(increased) {
+      console.log("Hello!");
+  }
 }
